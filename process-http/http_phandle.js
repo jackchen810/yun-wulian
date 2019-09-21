4ee263939974c7b6319e73e691a4ab79292be2d7
chenzejun <jack_chen_mail@163.com>
2019-09-21 15:52:46 +0800

    chenzejun 20190921 fixed bug

--- process-http/http_phandle.js
+++ process-http/http_phandle.js
@@ -1,14 +1,16 @@
 'use strict';
 const fork = require('child_process').fork;
 const logger = require( '../logs/logs.js');
-
+//const config = require('config-lite');
 
 //console.log('[main] create http process...');
 
 //创建一个工作进程
 const http_p = fork('./process-http/http_main.js');
 http_p.on('message', function () {
+    //config.process.http_pid = process.pid;
     console.log('[main] recv http message, pid =', process.pid);
+    //console.log('[main] recv http message, pid =', config.process.http_pid);
 });
 
 
@@ -23,7 +25,9 @@ http_p.on('exit', (err) => {
 http_p.on('close', (err) => {
     logger.error('http close:', err);
     //异常直接退出主进程，外部pm2重启整个进程
+    //这里要杀死其它子进程
     process.exit(1);
+    //process.kill();
 });
 
 
